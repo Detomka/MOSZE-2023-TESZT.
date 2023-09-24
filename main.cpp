59bf206e1a85cbd888616dea8f372606a3421105
Detomka <145669263+Detomka@users.noreply.github.com>
2023-09-24 10:54:37 +0200

    Update main.cpp
    Hibák meghatározása.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];                 //NELEMENTS--> N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'          // SZÖVEGET IDÉZŐJELBE KELL RAKNI, ";" A SOR VÉGÉRE
+    for (int i = 0;)                               // KELL EGY VÁLTOZÓ AMIVEL FELTÖLTJÜK A TÖMBÖT, FOR CIKLUSNÁL MEGKELL ADNI MEDDIG MEGYÜNK ÉS MILYEN LÉPÉSKÖZZEL
     {
-        b[i] = i * 2;
+        b[i] = i * 2;                    // A KORÁBBAN LÉTREHOZOTT VÁLTOZÓ ÉRTÉKÉT KELL 2 VEL MEGSZOROZNI NEM i-t , ezután növelni a vátozó értékét
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)            // nincs megadva meddig megyünk i vel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"           // nem helyes a formátum és nincsenek kiíratva az értékek
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)      // vessző helyett ";"
     {
-        atlag += b[i]
+        atlag += b[i]                         // ";" sor végére
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
