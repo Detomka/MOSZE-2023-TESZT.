765e1c14b2b08e7a9355b02486ca16f17e410c8e
Detomka <145669263+Detomka@users.noreply.github.com>
2023-09-24 10:59:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];                 //NELEMENTS--> N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'          // SZÖVEGET IDÉZŐJELBE KELL RAKNI, ";" A SOR VÉGÉRE
-    for (int i = 0;)                               // KELL EGY VÁLTOZÓ AMIVEL FELTÖLTJÜK A TÖMBÖT, FOR CIKLUSNÁL MEGKELL ADNI MEDDIG MEGYÜNK ÉS MILYEN LÉPÉSKÖZZEL
+    int *b = new int[N_ELEMENTS];                 //NELEMENTS--> N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa";       // SZÖVEGET IDÉZŐJELBE KELL RAKNI, ";" A SOR VÉGÉRE
+   int szamok = 1;
+    for (int i = 0;i < N_ELEMENTS ; i++)          // KELL EGY VÁLTOZÓ AMIVEL FELTÖLTJÜK A TÖMBÖT, FOR CIKLUSNÁL MEGKELL ADNI MEDDIG MEGYÜNK ÉS MILYEN LÉPÉSKÖZZEL
     {
-        b[i] = i * 2;                    // A KORÁBBAN LÉTREHOZOTT VÁLTOZÓ ÉRTÉKÉT KELL 2 VEL MEGSZOROZNI NEM i-t , ezután növelni a vátozó értékét
-    }
-    for (int i = 0; i; i++)            // nincs megadva meddig megyünk i vel
+        b[i] = szamok * 2;
+        szamok++;
+    for (int i = 0; i < N_ELEMENTS; i++)            // nincs megadva meddig megyünk i vel
     {
-        std::cout << "Ertek:"           // nem helyes a formátum és nincsenek kiíratva az értékek
+        std::cout << "Ertek:\t" << b[i]<< "\n";          // nem helyes a formátum és nincsenek kiíratva az értékek
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)      // vessző helyett ";"
+    for (int i = 0; i < N_ELEMENTS; i++)      // vessző helyett ";"
     {
-        atlag += b[i]                         // ";" sor végére
+        atlag += b[i] ;                        // ";" sor végére
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
